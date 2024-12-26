1cea4e3e03f74de1c97d8601d5ca1ab3aced3ea8
Johan Kreivi <johan.kreivi@sogeti.se>
2024-12-26 04:21:57 +0100

    1.1 done

---
+++ days/1.1.py
@@ -0,0 +1,32 @@
+
+import re
+from shlex import join
+
+puzzleInput = open(r"input/1.txt", "r").read()
+
+regX = r"((?P<leftList>\d+)* +(?P<rightList>\d+)*)"
+
+fullIter = re.finditer(regX, puzzleInput)
+
+fullMatch = list(fullIter)
+
+leftSide = []
+rightSide = []
+
+for m in fullMatch:
+    left = m.group("leftList")
+    right = m.group("rightList")
+    leftSide.append(left)
+    rightSide.append(right)
+
+leftSide.sort()
+rightSide.sort()
+combined = zip(leftSide, rightSide)
+
+distsum = 0
+for n in combined:
+    dist = abs(int(n[0]) - int(n[1]))
+    distsum += dist
+
+
+print(distsum)
